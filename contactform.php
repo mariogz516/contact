52047dfbb855afbd78df897a2148b2f2bf080c29
mariogz516 <65144792+mariogz516@users.noreply.github.com>
2020-08-30 07:30:38 -0400

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -5,6 +5,7 @@ $subject = $_POST['subject'];
 $mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
+$email_From = 'mariogz516@gmail.com';
 $mailTo = "bhc-test1@outlook.com";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an email from ".$name.".\n\n".$message;
