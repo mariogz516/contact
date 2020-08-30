f3a2b2e4ad4919255ba6bb36972481b73b8f57ac
mariogz516 <65144792+mariogz516@users.noreply.github.com>
2020-08-30 07:26:27 -0400

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -1,6 +1,5 @@
 <?php
 
-if (isset($_POST['submit'])){
 $name = $_POST['name'];
 $subject = $_POST['subject'];
 $mailFrom = $_POST['mail'];
@@ -13,6 +12,5 @@ $txt = "You have received an email from ".$name.".\n\n".$message;
 mail($mailTo, $subject, $txt, $headers);
 header("Location: index.html");
 
-}
 
 ?>
